3e36b899d166a2b3e21dc4420a9194278071052f
extact-io <ozytso@gmail.com>
2025-03-31 10:05:58 +0900

    update

--- platform-fw/src/main/java/io/extact/msa/spring/platform/fw/infrastructure/persistence/jpa/JpaRepositoryDelegator.java
+++ platform-fw/src/main/java/io/extact/msa/spring/platform/fw/infrastructure/persistence/jpa/JpaRepositoryDelegator.java
@@ -3,10 +3,11 @@ package io.extact.msa.spring.platform.fw.infrastructure.persistence.jpa;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
-public interface JpaRepositoryDelegator<E> extends JpaRepository<E, Integer> {
+public interface JpaRepositoryDelegator<E> extends JpaRepository<E, Integer>, JpaSpecificationExecutor<E> {
 
     List<E> findAllByOrderByIdAsc();
 }
